6a56b6ff2d87f32be1cc5e3496c8580fc87f0c09
cristian-rec <bitrec.61@gmail.com>
2024-06-19 11:44:41 -0300

    Agrego desarrollo inicial de controladores y rutas

---
+++ backend/routes/products.js
@@ -0,0 +1,9 @@
+import {getProducts, getCategory, getOrderDetail, getOrder} from '../controllers/controllers'
+
+const productsRoute = Router();
+const categoriesRoute = Router();
+const orderDetailRoute = Router();
+const orderRoute = Router();
+
+
+
