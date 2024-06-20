7bbe5fff6aeb5d1bbdd534b5f39a05cd25ba0803
Gerpidot <89492776+Gerpidot@users.noreply.github.com>
2024-06-20 12:01:48 -0300

    controllers users y sus rutas, implementacion en server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const errorHandler = require('./middlewares/errorHandler')
-const productsRouter = require('./routes/products-router')
-const categoriesRouter = require('./routes/categories-router')
-const ordersRouter = require('./routes/orders-router');
+const errorHandler = require("./middlewares/errorHandler");
+const productsRouter = require("./routes/products-router");
+const categoriesRouter = require("./routes/categories-router");
+const ordersRouter = require("./routes/orders-router");
 const orderDetailRouter = require("./routes/orderDetail-router");
-
+const usersRouter = require("./routes/users-router");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,12 +14,13 @@ app.use(express.json());
   res.send("¡Buenas team! Probando");
 }); */
 
-app.use('/api', productsRouter);
-app.use('/api', categoriesRouter);
-app.use('/api', ordersRouter);
+app.use("/api", productsRouter);
+app.use("/api", categoriesRouter);
+app.use("/api", ordersRouter);
 app.use("/api", orderDetailRouter);
+app.use("/api", usersRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
