d3f11b0c7ffcd655a3a256fc67421cbc95aa1626
Gerpidot <89492776+Gerpidot@users.noreply.github.com>
2024-06-28 15:28:24 -0300

    carrito alive

---
+++ frontend/src/scripts/carroCompras.js
@@ -0,0 +1,205 @@
+const mockedCarroEjemplo = [
+  {
+    producto: {
+      cover:
+        "https://res.cloudinary.com/dtrs7cus2/image/upload/v1719066580/age-of-empires_II_DE_fr43vy.jpg",
+      developer: "Ensemble Studios",
+      discount_price: null,
+      game_description:
+        "Un clásico juego de estrategia en tiempo real que regresa con gráficos mejorados en 4K, nuevo contenido y algunas mejoras en la jugabilidad.",
+      id_category: 1,
+      id_product: 1,
+      price: "6.99",
+      title: "Age of Empires II Definitive Edition",
+    },
+    cantidad: 0,
+  },
+];
+
+const container = document.getElementById("cart-list");
+const resumeContainer = document.getElementById("items-list");
+
+const validateCarroOrCreateon = () => {
+  let carroCompras = [];
+  // Recuperar la cadena JSON desde localStorage
+  let storedObjetos = localStorage.getItem("carroDeCompras");
+  let arrayObjetosRecuperado = JSON.parse(storedObjetos);
+  //si ya exsites y tiene algo
+  if (arrayObjetosRecuperado && arrayObjetosRecuperado.length > 0) {
+    return;
+  } else {
+    //lo creo en el local
+    localStorage.setItem("carroDeCompras", JSON.stringify(carroCompras));
+  }
+};
+validateCarroOrCreateon();
+
+export const obtenerCarroDelLocal = () => {
+  // Recuperar la cadena JSON desde localStorage
+  let storedObjetos = localStorage.getItem("carroDeCompras");
+
+  // Convertir de nuevo a un array de objetos usando JSON.parse()
+  let arrayObjetosRecuperado = JSON.parse(storedObjetos);
+  return arrayObjetosRecuperado;
+};
+
+export function mostrarCarro() {
+  try {
+    let allProductsonCart = obtenerCarroDelLocal();
+    container.innerHTML = "";
+    resumeContainer.innerHTML = "";
+    if (allProductsonCart.length === 0) {
+      const mensaje = document.createElement("div");
+      mensaje.classList.add("cart-item");
+      mensaje.innerHTML = `
+                      <div class="card" style="color:white;padding:25px;">
+                        No se registró ningun producto en tu carro :D
+                      </div>
+                    `;
+      container.appendChild(mensaje);
+      resumeContainer.innerHTML = "";
+      return;
+    }
+
+    allProductsonCart.forEach((producto, index) => {
+      //lista prods
+      const card = document.createElement("div");
+      card.classList.add("cart-item");
+      card.innerHTML = `
+                        <div class="item-description">
+                  <img
+                    src="${producto.producto.cover}"
+                    alt="${producto.producto.title}"
+                  />
+                  <div class="item-details">
+                    <h3>${producto.producto.title}</h3>
+                    <ul>
+                      <li>Genero: Fun</li>
+                      <li>Otros títulos: No modelado</li>
+                      <li>Calificación: No modelado</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="item-details_commercial">
+                  <div>
+                    <div>
+                      <h4>Precio:</h4>
+                      <p>${producto.producto.price}</p>
+                    </div>
+                    <div>
+                      <h4>Cantidad:</h4>
+                      <p>${producto.cantidad}</p>
+                    </div>
+                  </div>
+                  <div class="item-buttons">
+                    <button id="delete-button${index}" class="delete-button">
+                      <i class="bx bx-trash"></i>
+                    </button>
+                    <button id="decrement-button${index}" class="decrement-button">
+                      <i class="bx bx-minus"></i>
+                    </button>
+                    <button id="increment-button${index}" class="increment-button">
+                      <i class="bx bx-plus"></i>
+                    </button>
+                  </div>
+                  </div>`;
+      container.appendChild(card);
+      container.appendChild(document.createElement("hr"));
+      const incrementBtn = document.getElementById(`increment-button${index}`);
+      const decremenetBtn = document.getElementById(`decrement-button${index}`);
+      const deleteBtn = document.getElementById(`delete-button${index}`);
+      incrementBtn.addEventListener("click", () => {
+        modificarCantidad(producto.producto.id_product, true);
+      });
+      decremenetBtn.addEventListener("click", () => {
+        modificarCantidad(producto.producto.id_product, false);
+      });
+      deleteBtn.addEventListener("click", () => {
+        eliminarProducto(producto.producto.id_product);
+      });
+      //lista del resumen
+      const listItem = document.createElement("li");
+      listItem.classList.add("list-item");
+      listItem.innerHTML = `
+       <span>${producto.producto.title}</span>
+                  <span>$${(
+                    producto.producto.price * producto.cantidad
+                  ).toFixed(2)}</span>
+      `;
+
+      resumeContainer.appendChild(listItem);
+    });
+    const divBotones = document.createElement("div");
+    divBotones.innerHTML = ` <button>Confirmar compra</button>
+                <button id="reset-cart">Limpiar carro</button>`;
+    resumeContainer.appendChild(divBotones);
+    document.getElementById("reset-cart").addEventListener("click", () => {
+      return limpiarCarro();
+    });
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+  }
+}
+
+export function agregarAlCarro(productoAdd) {
+  const carroDeCompras = obtenerCarroDelLocal();
+
+  // Verificar si el producto ya está en el carro
+  let index = carroDeCompras.findIndex(
+    (item) => item.producto.id_product === productoAdd.id_product
+  );
+
+  if (index !== -1) {
+    // Si el producto ya existe, aumentar la cantidad
+    carroDeCompras[index].cantidad++;
+  } else {
+    // Si es un producto nuevo, agregarlo al carro
+    carroDeCompras.push({
+      producto: productoAdd,
+      cantidad: 1,
+    });
+  }
+
+  console.log(carroDeCompras);
+  // Actualizar el localStorage
+  localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+}
+
+function modificarCantidad(idProducto, masOmenos) {
+  let carroDeCompras = obtenerCarroDelLocal();
+  //obtener el indice del producto
+  let index = carroDeCompras.findIndex(
+    (item) => item.producto.id_product === idProducto
+  );
+
+  if (masOmenos) {
+    carroDeCompras[index].cantidad++;
+  } else {
+    if (carroDeCompras[index].cantidad > 1) {
+      carroDeCompras[index].cantidad--;
+    }
+  }
+
+  // Actualizar el localStorage
+  localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+  mostrarCarro();
+}
+
+const eliminarProducto = (idProducto) => {
+  let carroDeCompras = obtenerCarroDelLocal();
+  //obtener el indice del producto
+  let index = carroDeCompras.findIndex(
+    (item) => item.producto.id_product === idProducto
+  );
+
+  carroDeCompras.splice(index, 1);
+  localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+  mostrarCarro();
+};
+
+// Función para limpiar el carro de compras en localStorage
+function limpiarCarro() {
+  let carroDeCompras = [];
+  localStorage.setItem("carroDeCompras", JSON.stringify(carroDeCompras));
+  location.reload();
+}
--- frontend/src/scripts/products.js
+++ frontend/src/scripts/products.js
@@ -13,16 +13,32 @@ const mocked = {
   title: "Age of Empires II Definitive Edition",
 };
 import { getAllProducts } from "../services/products";
-
+import { agregarAlCarro } from "./carroCompras";
 const container = document.getElementById("grid-container-cards");
 const searchInput = document.getElementById("search-input");
 const resetButton = document.getElementById("reset-button");
 
 let allProducts = [];
+let carroCompras = [];
+
+const validateCarroOrCreateon = () => {
+  // Recuperar la cadena JSON desde localStorage
+  let storedObjetos = localStorage.getItem("carroDeCompras");
+  let arrayObjetosRecuperado = JSON.parse(storedObjetos);
+  //si ya exsites y tiene algo
+  if (arrayObjetosRecuperado && arrayObjetosRecuperado.length > 0) {
+    return;
+  } else {
+    //lo creo en el local
+    localStorage.setItem("carroDeCompras", JSON.stringify(carroCompras));
+  }
+};
+validateCarroOrCreateon()
+
 document.addEventListener("DOMContentLoaded", async function () {
   try {
     allProducts = await getAllProducts();
-
+    localStorage.setItem("productos", JSON.stringify(allProducts)); //paso los productos al localstorage, por las dudas jaja
     // Limpia el contenedor antes de mostrar nuevos productos
     container.innerHTML = "";
 
@@ -37,11 +53,13 @@ document.addEventListener("DOMContentLoaded", async function () {
       container.appendChild(mensaje);
     }
 
-    allProducts.forEach((producto) => {
+    allProducts.forEach((producto, index) => {
       const card = document.createElement("div");
       card.classList.add("border-card");
+      card.id = "game-card";
       card.innerHTML = `
         <div class="card">
+        <button id="add-button-${index}" class="add-button">+</button>
           <img src="${producto.cover}" alt="${producto.title}" class="cover"   loading="lazy" />
           <div class="description">
             <h2 class="title-card">${producto.title}</h2>
@@ -50,6 +68,11 @@ document.addEventListener("DOMContentLoaded", async function () {
         </div>
       `;
       container.appendChild(card);
+      const addCartItem = document.getElementById(`add-button-${index}`);
+
+      addCartItem.addEventListener("click", () => {
+        agregarAlCarro(producto);
+      });
     });
   } catch (error) {
     console.error("Error al obtener productos:", error);
@@ -73,11 +96,13 @@ const displayProducts = (products) => {
       container.appendChild(mensaje);
     }
 
-    products.forEach((producto) => {
+    products.forEach((producto, index) => {
       const card = document.createElement("div");
       card.classList.add("border-card");
+      card.id = "game-card";
       card.innerHTML = `
         <div class="card">
+        <button id="add-button-${index}" class="add-button">+</button>
           <img src="${producto.cover}" alt="${producto.title}" class="cover"   loading="lazy" />
           <div class="description">
             <h2 class="title-card">${producto.title}</h2>
@@ -86,6 +111,11 @@ const displayProducts = (products) => {
         </div>
       `;
       container.appendChild(card);
+      const addCartItem = document.getElementById(`add-button-${index}`);
+
+      addCartItem.addEventListener("click", () => {
+        agregarAlCarro(producto);
+      });
     });
   } catch (error) {
     console.error("Error al mostrar productos:", error);
@@ -96,6 +126,11 @@ const displayProducts = (products) => {
 searchInput.addEventListener("input", () => {
   try {
     const searchText = searchInput.value.trim().toLowerCase();
+    if (searchText.length === 0) {
+      displayProducts(allProducts);
+      document.getElementById("reset-button").style.display = "none";
+      return;
+    }
     document.getElementById("reset-button").style.display = "block";
 
     // Filtrar productos según el texto de búsqueda
@@ -113,8 +148,8 @@ searchInput.addEventListener("input", () => {
 });
 
 resetButton.addEventListener("click", () => {
-  searchInput.value = "";//borra el contenido del input
+  searchInput.value = ""; //borra el contenido del input
   resetButton.style.display = "none"; //se esconde el boton para hacer reset en el input
   searchInput.focus(); //hace foco en el input de search
-  displayProducts(allProducts);// muestra todos los productos nuevamente
+  displayProducts(allProducts); // muestra todos los productos nuevamente
 });
